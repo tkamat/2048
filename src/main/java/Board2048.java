c65737f07597dde11da0a7534bed18cc0208061c
Tushaar Kamat <platypusdiamond@gmail.com>
2017-05-06 15:26:02 -0400

    Update files

--- src/main/java/Board2048.java
+++ src/main/java/Board2048.java
@@ -1,5 +1,8 @@
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+
 /**
  * 2048 board
  *
@@ -8,28 +11,84 @@ import java.util.Random;
 public class Board2048 {
 
     private int[][] board;
+    private static final int BOARD_SIZE = 4;
 
     /**
      * Constructor for objects of Board2048 class
      */
     public Board2048() {
-        this.board = new int[4][4];
-        Random rand = new Random();
-        Point a = new Point(rand.nextInt(4), rand.nextInt(4));
-        Point b = new Point(rand.nextInt(4), rand.nextInt(4));
-        Point c = new Point(rand.nextInt(4), rand.nextInt(4));
-
-        this.assignStartingValues(a, b, c);
+        this.board = new int[BOARD_SIZE][BOARD_SIZE];
+        this.assignStartingNumbers(getRandomPoint(), getRandomPoint(), getRandomPoint());
     }
 
     public int[][] getBoard() {
         return this.board;
     }
 
-    public void assignStartingValues(Point a, Point b, Point c) {
+    /**
+     * Returns a randomly selected point on the board
+     *
+     * @return random point on the board
+     */
+    public static Point getRandomPoint() {
+        Random rand = new Random();
+        return new Point(rand.nextInt(BOARD_SIZE), rand.nextInt(BOARD_SIZE));
+    }
+
+    public void clearBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    /**
+     *
+     *
+     * @param a
+     *            first point
+     * @param b
+     *            second point
+     * @param c
+     *            third point
+     */
+    public void assignStartingNumbers(Point a, Point b, Point c) {
         board[a.getRow()][a.getCol()] = 2;
         board[b.getRow()][b.getCol()] = 2;
         board[c.getRow()][c.getCol()] = 2;
     }
-}
 
+    /**
+     * Adds a new number to board
+     *
+     * @param a
+     *            Point where mum is added
+     * @param num
+     *            value of number to be added
+     */
+    public void addNewNumber(Point a, int num) {
+        board[a.getRow()][a.getCol()] = num;
+    }
+
+    /**
+     * Adds a number to the edge of the 2048 board
+     *
+     * @param num
+     *            value of number to add
+     */
+    public void addNumberFromEdge(int num) {
+        List<Point> possiblePoints = new ArrayList<Point>();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (i == 0 || j == 0 || i == board.length - 1 || j == board[0].length - 1) {
+                    if (board[i][j] == 0)
+                        possiblePoints.add(new Point(i, j));
+                }
+            }
+        }
+
+        Random rand = new Random();
+        addNewNumber(possiblePoints.get(rand.nextInt(possiblePoints.size())), num);
+    }
+}
--- src/test/java/Board2048Test.java
+++ src/test/java/Board2048Test.java
@@ -1,26 +1,23 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import java.util.Random;
-
-
 public class Board2048Test {
-
-    private Board2048 test = new Board2048();
 
     @Test
     public void testBoardHasArray() {
+        Board2048 test = new Board2048();
         int[][] board = test.getBoard();
         assertEquals(board.length, 4);
         assertEquals(board[0].length, 4);
     }
 
     @Test
-    public void testStartingValuesAssigned() {
+    public void testStartingNumbers() {
+        Board2048 test = new Board2048();
         Point a = new Point(1, 2);
         Point b = new Point(0, 3);
         Point c = new Point(3, 2);
-        test.assignStartingValues(a, b, c);
+        test.assignStartingNumbers(a, b, c);
         int[][] board = test.getBoard();
         assertEquals(board[1][2], 2);
         assertEquals(board[0][3], 2);
@@ -28,9 +25,47 @@ public class Board2048Test {
     }
 
     @Test
-    public void testAddingNewValues() {
-
+    public void testAddingNewNumbers() {
+        Board2048 test = new Board2048();
+        Point addPoint = new Point(3, 3);
+        test.addNewNumber(addPoint, 2);
+        assertEquals(test.getBoard()[3][3], 2);
     }
 
-}
+    @Test
+    public void testClearBoard() {
+        Board2048 test = new Board2048();
+        test.clearBoard();
+        for (int i = 0; i < test.getBoard().length; i++) {
+            for (int j = 0; j < test.getBoard()[0].length; j++) {
+                assertEquals(test.getBoard()[i][j], 0);
+            }
+        }
+    }
 
+    @Test
+    public void testAddNumberFromEdge() {
+        Board2048 test = new Board2048();
+        test.clearBoard();
+        test.addNumberFromEdge(2);
+        for (int i = 0; i < test.getBoard().length; i++) {
+            for (int j = 0; j < test.getBoard()[0].length; j++) {
+                if (test.getBoard()[i][j] == 2) {
+                    assertEquals(
+                            (i == 0 || j == 0 || i == test.getBoard().length - 1 || j == test.getBoard().length - 1),
+                            true);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testMovePoint() {
+        Board2048 test = new Board2048();
+    }
+
+    @Test
+    public void testMoveLeft() {
+        Board2048 test = new Board2048();
+    }
+}
