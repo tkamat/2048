3517f0abbba1f26b51e10bcebcc8bb59bdfab941
Tushaar Kamat <platypusdiamond@gmail.com>
2017-05-12 00:06:43 -0400

    Added user interface

---
+++ src/main/java/com/tkamat/ui/GUI2048.java
@@ -0,0 +1,147 @@
+package com.tkamat.ui;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.HashMap;
+
+import javax.swing.*;
+
+import com.tkamat.game.Board2048;
+
+/**
+ * UI for 2048 game
+ *
+ * @author Tushaar Kamat
+ */
+public class GUI2048 extends JFrame {
+
+    private HashMap<Integer, Color> colorMap;
+    private Board2048 board;
+    private JLabel[][] tiles;
+
+    /**
+     * Constructor for GUI class.
+     */
+    public GUI2048(String name) {
+        super(name);
+
+        this.initiaizeColorMap();
+        board = new Board2048();
+
+        tiles = createTiles();
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        for (int i = 0; i < tiles.length; i++) {
+            for (int j = 0; j < tiles[0].length; j++) {
+                panel.add(tiles[i][j]);
+            }
+        }
+
+        panel.getInputMap().put(KeyStroke.getKeyStroke("H"), "moveLeft");
+        panel.getActionMap().put("moveLeft", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                board.moveLeft();
+                for (int i = 0; i < tiles.length; i++) {
+                    for (int j = 0; j < tiles.length; j++) {
+                        if (board.getBoard()[i][j] > 0) {
+                            tiles[i][j].setText("" + board.getBoard()[i][j]);
+                        }
+                        else {
+                            tiles[i][j].setText("");
+                        }
+                        tiles[i][j].setBackground(colorMap.get(new Integer(board.getBoard()[i][j])));
+                    }
+                }
+            }
+
+        });
+        panel.getInputMap().put(KeyStroke.getKeyStroke("L"), "moveRight");
+        panel.getActionMap().put("moveRight", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                board.moveRight();
+                for (int i = 0; i < tiles.length; i++) {
+                    for (int j = 0; j < tiles.length; j++) {
+                        if (board.getBoard()[i][j] > 0) {
+                            tiles[i][j].setText("" + board.getBoard()[i][j]);
+                        }
+                        else {
+                            tiles[i][j].setText("");
+                        }
+                        tiles[i][j].setBackground(colorMap.get(new Integer(board.getBoard()[i][j])));
+                    }
+                }
+            }
+        });
+        panel.getInputMap().put(KeyStroke.getKeyStroke("K"), "moveUp");
+        panel.getActionMap().put("moveUp", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                board.moveUp();
+                for (int i = 0; i < tiles.length; i++) {
+                    for (int j = 0; j < tiles.length; j++) {
+                        if (board.getBoard()[i][j] > 0) {
+                            tiles[i][j].setText("" + board.getBoard()[i][j]);
+                        }
+                        else {
+                            tiles[i][j].setText("");
+                        }
+                        tiles[i][j].setBackground(colorMap.get(new Integer(board.getBoard()[i][j])));
+                    }
+                }
+            }
+
+        });
+        panel.getInputMap().put(KeyStroke.getKeyStroke("J"), "moveDown");
+        panel.getActionMap().put("moveDown", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                board.moveDown();
+                for (int i = 0; i < tiles.length; i++) {
+                    for (int j = 0; j < tiles.length; j++) {
+                        if (board.getBoard()[i][j] > 0) {
+                            tiles[i][j].setText("" + board.getBoard()[i][j]);
+                        }
+                        else {
+                            tiles[i][j].setText("");
+                        }
+                        tiles[i][j].setBackground(colorMap.get(new Integer(board.getBoard()[i][j])));
+                    }
+                }
+            }
+        });
+
+        this.setSize(400, 400);
+        this.add(panel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    private JLabel[][] createTiles() {
+        JLabel[][] tiles = new JLabel[Board2048.BOARD_SIZE][Board2048.BOARD_SIZE];
+        for (int i = 0; i < tiles.length; i++) {
+            for (int j = 0; j < tiles[0].length; j++) {
+                tiles[i][j] = new JLabel("", SwingConstants.CENTER);
+                tiles[i][j].setFont(new Font("Helvetica", Font.BOLD, 72));
+                tiles[i][j].setBackground(Color.LIGHT_GRAY);
+                tiles[i][j].setOpaque(true);
+                if (board.getBoard()[i][j] > 0) {
+                    tiles[i][j].setText("" + board.getBoard()[i][j]);
+                }
+                tiles[i][j].setBackground(colorMap.get(new Integer(board.getBoard()[i][j])));
+            }
+        }
+        return tiles;
+    }
+
+    private void initiaizeColorMap() {
+        colorMap = new HashMap<Integer, Color>();
+        colorMap.put(new Integer(0), Color.LIGHT_GRAY);
+        colorMap.put(new Integer(2), new Color(239, 190, 110));
+        colorMap.put(new Integer(4), new Color(232, 148, 11));
+        colorMap.put(new Integer(8), new Color(132, 83, 2));
+    }
+
+}
